f1928d9b81604c169ccaf194cabfc2c05db821bd
Joshua Masland-Sarani <j.masland@gmail.com>
2016-04-13 10:52:14 -0400

    httparty

--- nutrition_api.rb
+++ nutrition_api.rb
@@ -1,4 +1,4 @@
-require 'HTTParty'
+require "HTTParty"
 
 class NutritionAPI
   KEY = "DQTsYwZAqhsAlpqz2YbXmsXa2Z3eVwylM2KPfAKg"
@@ -6,13 +6,15 @@ class NutritionAPI
   REQUEST_TYPES = ["ndbno"]
   LIST_TYPES = { food: "f", all_nutrients: "n", special_nutrients: "ns", standard_release_nutrients: "nr", food_group: "g" }
 
-  def getList(list_type)
+  def self.getList(list_type)
     list = HTTParty.get("#{BASE_URL}&lt=#{LIST_TYPES[list_type]}")
     p list
   end
 
-  def request(request_type, request_value)
+  def self.request(request_type, request_value)
     response = HTTParty.get("#{BASE_URL}&#{request_type}=#{request_value}")
     p response
   end
 end
+
+NutritionAPI.getList(:food)
