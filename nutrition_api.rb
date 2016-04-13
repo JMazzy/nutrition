116a87787721670508c21211a996ab2da6f13b02
Joshua Masland-Sarani <j.masland@gmail.com>
2016-04-13 10:37:09 -0400

    switch to simple test script

---
+++ nutrition_api.rb
@@ -0,0 +1,21 @@
+require 'HTTParty'
+
+class NutritionAPI
+  include 'HTTParty'
+
+  KEY = "DQTsYwZAqhsAlpqz2YbXmsXa2Z3eVwylM2KPfAKg"
+  BASE_URL = "http://api.nal.usda.gov/ndb/reports/?api_key=#{KEY}"
+  REQUEST_TYPES = ["ndbno"]
+  LIST_TYPES = { food: "f", all_nutrients: "n", special_nutrients: "ns", standard_release_nutrients: "nr", food_group: "g" }
+
+  def getList(list_type)
+    list = HTTParty.get("#{BASE_URL}&lt=#{LIST_TYPES[list_type]}")
+    p list
+  end
+
+  def request(request_type, request_value)
+    response = HTTParty.get("#{BASE_URL}&#{request_type}=#{request_value}")
+    p response
+  end
+
+end
