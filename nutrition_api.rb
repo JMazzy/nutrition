078d3142406be847a3b022888fde8a38efdf3c11
Joshua Masland-Sarani <j.masland@gmail.com>
2016-04-13 11:06:46 -0400

    nutrient requests started

--- nutrition_api.rb
+++ nutrition_api.rb
@@ -1,20 +1,21 @@
-require "HTTParty"
+require "httparty"
 
 class NutritionAPI
   KEY = "DQTsYwZAqhsAlpqz2YbXmsXa2Z3eVwylM2KPfAKg"
-  BASE_URL = "http://api.nal.usda.gov/ndb/reports/?api_key=#{KEY}"
+  BASE_URL = "http://api.nal.usda.gov/ndb/"
   REQUEST_TYPES = ["ndbno"]
   LIST_TYPES = { food: "f", all_nutrients: "n", special_nutrients: "ns", standard_release_nutrients: "nr", food_group: "g" }
 
   def self.getList(list_type)
-    list = HTTParty.get("#{BASE_URL}&lt=#{LIST_TYPES[list_type]}")
+    list = HTTParty.get("#{BASE_URL}list?lt=#{LIST_TYPES[list_type]}&api_key=#{KEY}")
     p list
   end
 
   def self.request(request_type, request_value)
-    response = HTTParty.get("#{BASE_URL}&#{request_type}=#{request_value}")
+    response = HTTParty.get("#{BASE_URL}/reports/?#{request_type}=#{request_value}&api_key=#{KEY}")
     p response
   end
 end
 
 NutritionAPI.getList(:food)
+NutritionAPI.request("ndbno", "01001")
